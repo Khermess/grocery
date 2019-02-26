a4db11cdde0df63af58d16bfd0e49b28e7736a8f
S00451 <misupermail@mail.com>
2019-02-26 15:14:41 +0100

    Añadida la foreing de supermercados

--- app/Models/supermercados.php
+++ app/Models/supermercados.php
@@ -9,7 +9,6 @@ class supermercados extends Model
     //El nombre de la tabla
     protected $table="supermercados";
     
-    
     //esto es un accesor. Se define con el nombre del slug del campo descripcion_guay enmedio.
     //Se llama automaticamente cuando alguien intenta leer la columna descripcion_guay
     public function getDescripcionGuayAttribute($value){
@@ -21,4 +20,20 @@ class supermercados extends Model
     public function setDescripcionGuayAttribute($value){
         $this->attributes['descripcion_guay'] = strtolower($value);
     }
+    
+    
+    //Foreing de la tabla supermercados_nombre
+    public function nombre(){
+        return $this->hasMany('App\Models\supermercados_nombre', //tabla de donde viene la foreing
+                'supermercados_nombre_id', //clave foreing (opcional)
+                'supermercados_nombre_id'); //clave local (opcional)
+    }
+    
+    //Si hubiera alguna relacion manyTomany:
+    //In addition to customizing the name of the joining table, you may also 
+    //customize the column names of the keys on the table by passing additional 
+    //arguments to the belongsToMany method. The third argument is the foreign 
+    //key name of the model on which you are defining the relationship, while 
+    //the fourth argument is the foreign key name of the model that you are joining to:
+    //return $this->belongsToMany('App\Role', 'role_user', 'user_id', 'role_id');
 }
